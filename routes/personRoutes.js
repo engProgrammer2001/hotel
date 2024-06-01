8b27b713bc747941c2769356b7b935b4f95e9f69
engProgrammer2001 <askushwaha613@gmail.com>
2024-06-01 23:12:58 +0530

    check error

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Person = require("../models/Person");
+const Person = require("../models/Person.js");
 
 router.post("/", async (req, res) => {
   try {
